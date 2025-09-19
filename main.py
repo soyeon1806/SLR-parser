1584b7e84c3d88fd4ccf5b79881f73a6af228520
soyeon <ksy1118s@naver.com>
2025-09-19 19:51:20 +0900

    chore: 주석 오타 수정

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import syntax_analyzer
 import table_generator
 
 if __name__ == '__main__':
-    src_path = Path(sys.argv[1])           # inputs/01.txt
+    src_path = Path(sys.argv[1])
     out_dir = Path('outputs')
     out_dir.mkdir(exist_ok=True)
     out_file = out_dir / (src_path.stem + '_tree.txt')
 
-    # LR 테입르 로드
+    # LR 테이블 로드
     action_table, goto_table = table_generator.generate_table('lr_table.txt')
 
     # 문법 로드
